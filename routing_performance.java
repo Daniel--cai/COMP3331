e5c7c4b3ff42143587678d734431938a1bb35f35
Daniel Musso <dmus046@cse.unsw.EDU.AU>
2013-10-10 14:52:13 +1100

    added code for SDP

--- routing_performance.java
+++ routing_performance.java
@@ -11,11 +11,16 @@ public class routing_performance {
 
   // Maximum number of nodes   
   static final int MAX_NODES = 26;
+  
+  // Max propagation delay 
+  static final int MAX_DELAY = 200;
+  public static String routingProtocol;
 
   // Map of edge lists for each node
   public static Map<Character, List<Edge>> graph;
 
   public static List<Edge> edges;
+  public static Map<Pair, Edge> edgeMap;
   public static List<Character> nodes;
 
   public static Set<Character> usedNodes;
@@ -29,9 +34,9 @@ public class routing_performance {
 
   public static int numVCRequests = 0;
   public static int numSuccessRequests = 0;
-  public static int percentageSuccessRequests = 0;
+  public static float percentageSuccessRequests = 0;
   public static int numBlockedRequests = 0;
-  public static int percentageBlockedRequests = 0;
+  public static float percentageBlockedRequests = 0;
 
   public static void main(String[] args) {
 
@@ -53,15 +58,23 @@ public class routing_performance {
 
     if (args[0].equals("SHP")) {
       System.out.println("SHP");
+      routingProtocol = "SHP";
+      SHP(args[2]);
+    }
+    else if (args[0].equals("SDP")) {
+      System.out.println("SDP");
+      routingProtocol = "SDP";
       SHP(args[2]);
     }
 
+    percentageSuccessRequests = ((float)numSuccessRequests/numVCRequests * 100);
+    percentageBlockedRequests = ((float)numBlockedRequests/numVCRequests * 100);
     System.out.println("total number of virtual circuit requests: " + numVCRequests);
-    numSuccessRequests = numVCRequests;
+    //numSuccessRequests = numVCRequests;
     System.out.println("number of successfully routed requests: " + numSuccessRequests);
-    System.out.println("percentage of successfully routed request: " + (numSuccessRequests/numVCRequests * 100));
+    System.out.println("percentage of successfully routed request: " + (int)percentageSuccessRequests);
     System.out.println("number of blocked requests: " + numBlockedRequests);
-    System.out.println("percentage of blocked requests: " + percentageBlockedRequests);
+    System.out.println("percentage of blocked requests: " + (int)percentageBlockedRequests);
 
 
 
@@ -71,8 +84,9 @@ public class routing_performance {
     graph = new HashMap<Character, List<Edge>>();
     edges = new ArrayList<Edge>();
     nodes = new ArrayList<Character>();
+    edgeMap = new HashMap<Pair, Edge>();
 
-//////////////////////////////////////////////////////////////////
+
 /*
     for (char ch = 'A'; ch <= 'Z'; ++ch) {
       graph.put(ch, new LinkedList<Edge>());
@@ -93,15 +107,25 @@ public class routing_performance {
         char adjNode = specs[1].charAt(0);
         int delay = Integer.parseInt(specs[2]);
         int capacity = Integer.parseInt(specs[3]);
-        Edge e = new Edge(node, adjNode, delay, capacity);
+        Edge e1 = new Edge(node, adjNode, delay, capacity);
         Edge e2 = new Edge(adjNode, node, delay, capacity);
-        edges.add(e);
+        edges.add(e1);
         edges.add(e2);
-
+        Pair p1 = new Pair(node,adjNode);
+        Pair p2 = new Pair(adjNode,node);
+        edgeMap.put(p1,e1);
+        edgeMap.put(p2,e2);  
+        Pair p3 = new Pair (node,adjNode);
+/*
+        if (edgeMap.containsKey(p3)) {
+          System.out.println("CONTAINS P3");
+        }
+*/
         // put nodes in graph and nodes list
         if (!graph.containsKey(node)) {
           graph.put(node, new LinkedList<Edge>());
           nodes.add(node);
+
         }
         if (!graph.containsKey(adjNode)) {
           graph.put(adjNode, new LinkedList<Edge>());
@@ -109,7 +133,7 @@ public class routing_performance {
         }
 
 
-        graph.get(node).add(e); 
+        graph.get(node).add(e1); 
         graph.get(adjNode).add(e2); // dont copy both directions to graph?
         //System.out.println(line);
         //numVCRequests++;
@@ -159,7 +183,7 @@ public class routing_performance {
     usedNodes = new HashSet<Character>();
     unusedNodes = new HashSet<Character>();
     List<Character> finalPath = new ArrayList<Character>();
-     
+    boolean success = false;
 
     unusedNodes.add(srcNode);
     distances = new HashMap<Character, Integer>();
@@ -173,13 +197,19 @@ public class routing_performance {
       usedNodes.add(node);
       unusedNodes.remove(node);
       if (node == destNode) {
+        ///////////////////////////////////////////////////
+        numSuccessRequests++;
+        success = true;
         break;
       }
+
       // now add adjacent nodes to unusedNodes
       checkAdjacentNodes(node);
       //findMinimalDistances(node);
     }
-
+    if (!success) {
+      numBlockedRequests++;
+    }
     System.out.println("distance from " + srcNode + " to " + destNode + " : " + distances.get(destNode));
     char n = destNode;
     System.out.println("Path from " + srcNode + " to " + destNode);
@@ -207,7 +237,12 @@ public class routing_performance {
 
   public static void initialiseDistances() {
     for (char ch = 'A'; ch <= 'Z'; ch++) {
-      distances.put(ch, MAX_NODES);
+      if (routingProtocol.equals("SHP")) {
+        distances.put(ch, MAX_NODES);
+      }
+      else if (routingProtocol.equals("SDP")) {
+        distances.put(ch, MAX_DELAY);
+      }
     }
   }
 
@@ -242,6 +277,7 @@ public class routing_performance {
 */
   public static void checkAdjacentNodes(char srcNode) {
     System.out.println("checkAdjacentNodes");
+/*
     for (Edge e : edges) {
       if (e.srcNode == srcNode && !usedNodes.contains(e.destNode)) { // then its adjacent
         
@@ -259,14 +295,42 @@ public class routing_performance {
         System.out.println("distance to : " + e.destNode + " from " + srcNode + " : " + distances.get(e.destNode));
       }
     }
+*/
+    for (char ch = 'A'; ch <= 'Z'; ch++) {
+      Pair p = new Pair(srcNode,ch);
+      if (edgeMap.containsKey(p) && !usedNodes.contains(ch)) {
+        int newdistance = 0;
+        if (routingProtocol.equals("SHP")) {
+          newdistance = 1 + distances.get(srcNode);
+        }
+        else if (routingProtocol.equals("SDP")) {
+          int linkCost = edgeMap.get(p).delay;
+          
+          newdistance = linkCost + distances.get(srcNode);
+        }
+        
+        
+        //if (newdistance < getShortestDistance(e.destNode)) {
+        if (newdistance < distances.get(ch)) {
+          distances.put(ch, newdistance);
+          unusedNodes.add(ch);
+          path.put(ch, srcNode); // ie you get to e.destNode from srcNode
+        }
+        
+        System.out.println(srcNode + "->" + ch);
+        //System.out.println("distance to : " + ch + " from " + srcNode + " : " + distances.get(ch));
+      }
+    }
+
   }
 }
 
 class Edge {
   public char srcNode;
   public char destNode;
-  public int delay;
-  public int capacity;
+  public final int delay;
+  public final int capacity;
+  public int load = 0;
 
   public Edge (char src, char dest, int d, int c) {
     this.srcNode = src;
@@ -280,6 +344,29 @@ class Edge {
   }
 }
 
+class Pair {
+  public final char first;
+  public final char second;
+
+  public Pair(char f, char s) {
+    this.first = f;
+    this.second = s;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    Pair p = (Pair) other;
+    return this.first == p.first && this.second == p.second;
+  }
+
+
+  @Override public int hashCode() {
+    int hash = 9;
+    hash = hash * 23 + first + second;
+    return hash;
+  }
+}
+/*
 class Node {
   public char name;
 
@@ -287,7 +374,7 @@ class Node {
     this.name = n;
   }
 }
-
+*/
 /*
     public void addNeighbour(char v1, char v2, int delay, int capacity) {
 
